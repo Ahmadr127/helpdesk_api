69b814909547476aced38a9d3aa511fbc120b94e
Ahmadr127 <ahmdrifai808@gmail.com>
2026-09-21 14:19:16 +0700

    asd

--- resources/views/user/administrasi-umum/order-perbaikan/create.blade.php
+++ resources/views/user/administrasi-umum/order-perbaikan/create.blade.php
@@ -80,7 +80,7 @@
                         @error('prioritas') <p class="text-red-500 text-xs mt-1">{{ $message }}</p> @enderror
                     </div>
 
-                    <div>
+                    <div class="hidden">
                         <label class="block text-sm font-medium text-gray-700 mb-1">Nama Barang <span class="text-red-500">*</span></label>
                         <input type="text" name="nama_barang" value="{{ old('nama_barang') }}" required placeholder="Nama barang / peralatan"
                                class="w-full px-3 py-2.5 border border-gray-300 rounded-lg text-sm focus:ring-2 focus:ring-green-500 focus:border-green-500">
